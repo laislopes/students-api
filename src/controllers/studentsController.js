2aa01045e2fe9e8d188b240f54e1fa164f96ccf2
laislopes <laisjkl@hotmail.com>
2023-07-14 18:07:03 -0300

    Add dotenv into the project and implements Student CRUD.

--- src/controllers/studentsController.js
+++ src/controllers/studentsController.js
@@ -1,13 +1,65 @@
+import students from "../models/Student.js";
+
 class StudentsController {
 
-    static getAll = async(req, res, next) => {
+    static getAll = async(_, res) => {
         try {
-            
-            next();
+            const result = await students.find();
+
+            res.status(200).send(result);
         } catch (error) {
-            next(error)
+            console.log(error);
         }
     }; 
+
+    static getById = async(req, res) => {
+        try {
+            const { id } = req.params;
+
+            const student = await students.findById(id);
+
+            if(!student) res.status(200).send(student);
+        } catch (error) {
+            console.log(error);
+        }
+    }; 
+
+    static add = async(req, res) => {
+        try {
+            let student = new students(req.body);
+
+            await student.save();
+
+            res.status(201).send(student.toJSON());
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    static update = async(req, res) => {
+        try {
+            const { id } = req.params;
+
+            const studentResult = await students.findByIdAndUpdate(id, { $set: req.body });
+
+            if(!studentResult) res.status(200).send({message: `The book ${id} has been updated successfully!`});
+        } catch (error) {
+            console.log(error);
+        }
+    }; 
+
+    static delete = async(req, res) => {
+        try {
+            const { id } = req.params;
+
+            const studentResult = await students.findByIdAndDelete(id);
+
+            if(!studentResult) res.status(200).send({message: `The book ${id} has been deleted successfully!`});
+        } catch (error) {
+            console.log(error);
+        }
+    }; 
+
 }
 
 export default StudentsController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import students from "./studentsRoutes.js";
 
 const routes = (app) => {
 
@@ -7,7 +8,8 @@ const routes = (app) => {
     });
 
     app.use(
-        express.json()
+        express.json(),
+        students
     );
 };
 
