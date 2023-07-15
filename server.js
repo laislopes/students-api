77da066b50e704d38240ef655bb242834aecc913
laislopes <laisjkl@hotmail.com>
2023-07-15 10:13:27 -0300

    Changes server port.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import "dotenv/config.js";
 import app from "./src/app.js";
 
-const port = 3000;
+const port = 3333;
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
