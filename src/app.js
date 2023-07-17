ae00e1299236d24dca3c074f3aa3504d53f5cd65
laislopes <laisjkl@hotmail.com>
2023-07-17 16:08:34 -0300

    Configures CORS.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "./config/dbConnect.js";
+import cors from "cors";
 import errorsManager from "./middlewares/errorsManager.js";
 import notFoundManager from "./middlewares/notFoundManager.js";
 import routes from "./routes/index.js";
@@ -10,10 +11,13 @@ db.once("open", () => {
 });
 
 const app = express();
+app.use(cors());
+
 routes(app);
 
 app.use(notFoundManager);
 
 app.use(errorsManager);
+
 
 export default app;
